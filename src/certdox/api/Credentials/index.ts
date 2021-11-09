11cbb42d3c61b136dd11d4d50d51e20b33ce3977
Paulinho <qualvalordex@gmail.com>
2021-11-09 10:19:05 -0300

    🔒️ add credentials class

--- src/certdox/api/Credentials/index.ts
+++ src/certdox/api/Credentials/index.ts
@@ -1,31 +1,29 @@
-import { Redis } from '../../../common'
+import request from 'axios'
+import { CredentialsOptions } from "../../../common/GenericCredentials";
+import { GenericCredentials } from "../../../common/GenericCredentials";
 
-export interface CredentialsConfig {
-    login: string,
+export interface CertdoxJWT {
+    login: string
     password: string
 }
 
-export class Credentials {
-    private login: string
-    private password: string
+export interface CertdoxCredentialsObject {
+    default: CertdoxCredentials
+}
 
-    constructor({ login, password }: CredentialsConfig) {
-        this.login = login
-        this.password = password
+export class CertdoxCredentials extends GenericCredentials<CertdoxJWT> {
+    constructor(auth: CertdoxJWT, options?: CredentialsOptions) {
+        super(auth, options)
     }
 
     public async getToken(): Promise<string> {
-        // Do something
-        let token = ''
-        return token
-    }
-
-    private async isTokenValid(): Promise<boolean> {
-        // Do something more
-        return true
-    }
-
-    private async setToken(token: string, expiration: number): Promise<void> {
-        // More and more and more...
+        // if (this.redis && await this.isTokenValid()) {
+        //     return this.redis.retrieveToken()
+        // }
+        // Verificar a regra de negócio de autenticação
+        // ENDPOINT AUTENTICAÇÃO: https://sandbox-saas.docket.com.br/api/v2/auth/login
+        // const { data: { token, expiration } } = await request({ url: '', method: 'POST', data: {} })
+        // await this.setToken(token, expiration)
+        return process.env.CERTDOX_API_KEY || ''
     }
 }
