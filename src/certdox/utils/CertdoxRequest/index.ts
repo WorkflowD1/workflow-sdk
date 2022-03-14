255b6c6ccfb6553f1e049cae08c256114cf6e127
Wilson Fabozi <wilsonfabozi@gmail.com>
2022-03-14 16:37:53 -0300

    fix: :bug: baseURL adjustment

--- src/certdox/utils/CertdoxRequest/index.ts
+++ src/certdox/utils/CertdoxRequest/index.ts
@@ -24,11 +24,12 @@ export abstract class CertdoxRequest {
   }
 
   private async request(url: string, data: any) {
+    const baseURL = data.env === 'dev' ? this.baseUrlDev : this.baseUrlProd;
     let dataFiltered = data;
     delete dataFiltered.env;
 
     return request({
-      baseURL: data.env === 'dev' ? this.baseUrlDev : this.baseUrlProd,
+      baseURL,
       url,
       method: 'POST',
       headers: {
